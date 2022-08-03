01bbcdbceb48633830f5a1461e470f780c64f534
selemondev <selemondev19@gmail.com>
2022-08-03 19:19:54 +0300

    :white_check_mark: Add comments

--- Background-Color-Flipper/index.js
+++ Background-Color-Flipper/index.js
@@ -1,10 +1,12 @@
 const colors = ["#FF0000","#fff","#000","	#00FF00","	#2E8B57","	#00FFFF","	#008080","	#EE82EE","	#8B008B","#DC143C","#FF7F50"];
 let btnEl = document.querySelector("#btn-el");
 let colorEl = document.querySelector("#color-el");
+
+// get random number
 function getRandomNumber() {
     return Math.floor(Math.random() * colors.length)
 }
-console.log(getRandomNumber());
+
 btnEl.addEventListener("click",function(){
     const theme = getRandomNumber();
     console.log(theme)
